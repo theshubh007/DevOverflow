4c095bf85a9bec2709aa4bbfa41c2e76f35c99ed
shubham <thekingshubh07@gmail.com>
2024-08-20 11:02:59 -0700

    webhook2 updated

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -54,13 +54,12 @@ export async function POST(req: Request) {
   const eventType = evt.type;
 
   if (eventType === "user.created") {
-    const { id, email_addresses, image_url, username, first_name, last_name } =
-      evt.data;
+    const { id, email_addresses, image_url, first_name, last_name } = evt.data;
     // Create a new user in your database
     const mongoUser = await createUser({
       clerkId: id,
       name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
-      username: username!,
+      username: `${first_name}${last_name ? ` ${last_name}` : ""}`,
       email: email_addresses[0].email_address,
       picture: image_url,
     });
@@ -68,14 +67,13 @@ export async function POST(req: Request) {
   }
 
   if (eventType === "user.updated") {
-    const { id, email_addresses, image_url, username, first_name, last_name } =
-      evt.data;
+    const { id, email_addresses, image_url, first_name, last_name } = evt.data;
     // Create a new user in your database
     const mongoUser = await updateUser({
       clerkId: id,
       updateData: {
         name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
-        username: username!,
+        username: `${first_name}${last_name ? ` ${last_name}` : ""}`,
         email: email_addresses[0].email_address,
         picture: image_url,
       },
